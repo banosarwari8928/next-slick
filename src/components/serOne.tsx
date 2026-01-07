8e9f0f89efe48facd1129464989dcbaff4cedca9
banosarwari8928 <banosarwari8928@gmail.com>
2026-01-07 17:26:14 +0430

    worked on server component

---
+++ src/components/serOne.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function serTwo() {
+    console.log("Server One")
+  return (
+    <div>Hi I'm comming from Server Component One</div>
+  )
+}
+
+export default serTwo
---
+++ src/components/serTwo.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function serTwo() {
+    console.log("Server Two")
+  return (
+    <div>Hi I'm comming from Server Component Two</div>
+  )
+}
+
+export default serTwo
